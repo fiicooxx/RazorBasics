66c0a84f81415a975e0e1f768e29167cec4d5864
Filip Copija <filip.copija1@gmail.com>
2023-03-09 22:30:25 +0100

    Edit & Delete

---
+++ RazorWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorWeb.Data;
+using RazorWeb.Model;
+
+namespace RazorWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AppDbContext _context;
+        public DeleteModel(AppDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Category Category { get; set; }
+        public void OnGet(int id)
+        {
+            Category = _context.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "Values must be different");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
---
+++ RazorWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorWeb.Data;
+using RazorWeb.Model;
+
+namespace RazorWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _context;
+        public EditModel(AppDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Category Category { get; set; }
+        public void OnGet(int id)
+        {
+            Category = _context.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "Values must be different");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(Category);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
