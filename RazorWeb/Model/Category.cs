6cad0daf158d8750bf8a26f92ff8cc6adecad5ba
Filip Copija <filip.copija1@gmail.com>
2023-03-09 11:26:26 +0100

    Client Side Validations

--- RazorWeb/Model/Category.cs
+++ RazorWeb/Model/Category.cs
@@ -9,7 +9,8 @@ namespace RazorWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [DisplayName("Display Order")]
+        [Display(Name="Display Order")]
+        [Range(0,100, ErrorMessage = "Display Order must be from 0 to 100!")]
         public int DisplayOrder { get; set; }
     }
 }
